a60a19bbf7c615c957d7540c8e2f94f810960a10
x0oey6B8 <13136817+x0oey6B8@users.noreply.github.com>
2023-07-17 19:33:34 +0900

    update

--- app/src/scripts/global.ts
+++ app/src/scripts/global.ts
@@ -2,6 +2,7 @@ import { IKeyboard, Keyboard } from "./features/keyboard";
 import { IMouse, Mouse } from "./features/mouse";
 
 export interface IGlobal {
+    isInterruptionReqested: boolean;
     alert: (text: string) => void;
     wait: (time: number, highPrecision: boolean) => boolean;
     cancelWait: () => void;
@@ -16,18 +17,62 @@ export interface IGlobalFactory {
 export class GlobalFactory implements IGlobalFactory {
     create(): IGlobal {
         return {
+            isInterruptionReqested: false,
             keyboard: new Keyboard,
             mouse: new Mouse,
             // @ts-ignore
             alert: function (text: string) {
             },
+            // @ts-ignore
             wait: function (time, highPrecision) {
-                console.log(time, highPrecision);
                 return true;
             },
             cancelWait: function () {
-
             }
         }
     }
+}
+
+export class DebuggableGlobalFactory implements IGlobalFactory {
+    create(): IGlobal {
+        return {
+            isInterruptionReqested: false,
+            keyboard: new Keyboard,
+            mouse: new Mouse,
+            // @ts-ignore
+            alert: function (text: string) {
+                console.log(text);
+            },
+            // @ts-ignore
+            wait: function (time, highPrecision) {
+                console.log(`wait: ${time}`);
+            },
+            cancelWait: function () {
+                console.log("cancelWait()");
+            }
+        }
+    }
+}
+
+export interface IGlobalRegister {
+    register(global: IGlobal): void;
+}
+
+export class GlobalRegister implements IGlobalRegister {
+    register(global: IGlobal): void {
+        if (!global) {
+            return;
+        }
+
+        const w = (window as any);
+        const keys = Object.keys(global);
+        const values = Object.values(global);
+        console.log(values);
+        for (let index = 0; index < keys.length; index++) {
+            const key = keys[index];
+            const value = values[index];
+            w[key] = value;
+        }
+    }
+
 }
