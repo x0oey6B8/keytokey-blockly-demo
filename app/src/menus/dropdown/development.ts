1a2bb040d0c32de3d592ce73cbc6fe929f0542ed
x0oey6B8 <13136817+x0oey6B8@users.noreply.github.com>
2024-05-26 10:14:33 +0900

    コメントアウト

--- app/src/menus/dropdown/development.ts
+++ app/src/menus/dropdown/development.ts
@@ -1,14 +1,14 @@
 import { IDropDownMenuItem } from "../../models/dropdown";
-import { useBlocklyStore } from "../../stores/blocklyStore";
-import { useCommandPaletteStore } from "../../stores/commandPaletteStore";
-import * as BlocklyMenus from "../commands/blockly"
+// import { useBlocklyStore } from "../../stores/blocklyStore";
+// import { useCommandPaletteStore } from "../../stores/commandPaletteStore";
+// import * as BlocklyMenus from "../commands/blockly"
 
 export class TestMenuItem implements IDropDownMenuItem {
     header = "テスト";
     subHeader = "dev";
     condition = () => true;
     clicked = () => {
-        const blocklyStore = useBlocklyStore();
+        // const blocklyStore = useBlocklyStore();
 
     };
 }
